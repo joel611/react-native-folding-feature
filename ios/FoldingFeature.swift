faaa54ea4d6f968a07c93af09fc9039cf2031637
Joel Chan <chanhokyim@gmail.com>
2023-09-07 21:45:24 +0800

    chore: initial commit

---
+++ ios/FoldingFeature.swift
@@ -0,0 +1,8 @@
+@objc(FoldingFeature)
+class FoldingFeature: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
